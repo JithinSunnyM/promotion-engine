223b831793ac5a41fdd771add6b7b05d0e056be7
jithinmsunny <jithinsunnym@gmail.com>
2021-06-12 09:06:10 +0100

    Added unit price for SKU IDs

--- source/source/Program.cs
+++ source/source/Program.cs
@@ -8,5 +8,33 @@ namespace source
         {
             Console.WriteLine("Hello World!");
         }
+
+        // Unit price for SKU IDs 
+        public class UnitPrice
+        {
+            public string skuId { get; set; }
+            public decimal price { get; set; }
+
+            public UnitPrice(string id)
+            {
+                this.skuId = id;
+                switch (id)
+                {
+                    case "A": 
+                        this.price = 50;
+                        break;
+                    case "B":
+                        this.price = 30;
+                        break;
+                    case "C":
+                        this.price = 20;
+                        break;
+                    case "D":
+                        this.price = 15;
+                        break;
+                }
+            }
+        }
+
     }
 }
